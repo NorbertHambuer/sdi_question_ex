a036b80556b187c9dba4225711885c4cbf0ccd37
Norbert.Hambuer <hambuernorbi@yahoo.com>
2019-06-27 09:19:33 +0300

    Clear app

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://mjmhyauqsdypod:57b9341924f89ffa5a0a658714d448dd192d8fadaf7150caace887bab75ad9d4@ec2-79-125-126-205.eu-west-1.compute.amazonaws.com:5432/dk01ml16p5qig'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://postgres:ad@localhost:5432/chatbot-generator'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-#
-# migrate = Migrate(app, db)
-# manager = Manager(app)
 
 @app.route('/')
 def hello_world():
@@ -20,6 +9,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    # app.run()
-    #manager.run()
-    print("asd")
+    app.run()
